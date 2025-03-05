ea755835eeb693bf71e4ce1584e49d370ae8f4d5
AlondraVVH <alondragonzalez@liceovvh.cl>
2025-03-05 00:07:06 -0300

    ok

--- Sucursal Digital Consalud/script.js
+++
@@ -1,7 +0,0 @@
-// Alternar visibilidad de la contraseña
-document.querySelector('.toggle-password').addEventListener('click', function () {
-    const passwordInput = document.getElementById('password');
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    this.classList.toggle('fa-eye-slash');
-});
