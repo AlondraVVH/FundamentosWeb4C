67653456ad0d746e734f7e7276f5b416286b12ee
AlondraVVH <alondragonzalez@liceovvh.cl>
2025-03-12 09:30:12 -0300

    ok

--- JavaScript/04_ejercicio_variable/script.js
+++ JavaScript/04_ejercicio_variable/script.js
@@ -5,19 +5,18 @@ console.log("Vinculado correctamente...")
 
 function declararVariables() {
     // Tu código aquí
-    let ahora = 2025;   // Año actual
-    let fecha_nac = 2008; // Año de nacimiento
+
+    let ahora = parseInt ( prompt ("Ingrese Año Actual:")); // Año actual
+    let fecha_nac = parseInt ( prompt ("Ingrese Año de Nacimiento:"));
     let edad = ahora - fecha_nac;
     alert("Tu edad es: " + edad);
-    
-
     }
     
     // Ejercicio 2: Asignación de valores
     // Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
     function asignarValores() {
-    // Tu código aquí
-        let var1 = 10;
+        // Tu código aquí
+        let var1 = parseInt(prompt("Ingrese el valor de var1:"));
         let var2 = var1;    
         alert("El valor de var2 es: " + var2);
     }
@@ -27,28 +26,30 @@ function declararVariables() {
     // Suma 'num1','num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
     // Muestra los resultados en un mensaje.
     function realizarOperaciones() {
-    
-    alert("resultados" );
-        
-    // Tu código aquí
-    let num1 = 1;
-    let num2 = 2;       
-    let num3 = 5;
-    let suma = num1 + num2 + num3;
-    let resta = num1 - num3;
-    let multiplicacion = num2 * num3;
-    let division = num1 / 10;
-    alert("La suma es (1+2+5): " + suma);   
-    alert("La resta es (1-5): " + resta);
-    alert("La multiplicación es (2*3): " + multiplicacion);
-    alert("La división es (1/10): " + division);
-    
+        // Tu código aquí
+        let num1 = parseInt(prompt("Ingrese el primer número:"));
+        let num2 = parseInt(prompt("Ingrese el segundo número:"));
+        let num3 = parseInt(prompt("Ingrese el tercer número:"));
+        let suma = num1 + num2 + num3;
+        let resta = num1 - num3;
+        let multiplicacion = num2 * num3;
+        let division = num1 / 120;
+        let resultados = "La suma de los tres números es: " + suma + "\n" +
+        "La resta de num1 y num3 es: " + resta + "\n" +
+        "La multiplicación de num2 y num3 es: " + multiplicacion + "\n" +
+        "La división de num1 entre 120 es: " + division
+        alert(resultados);
     }
     
     // Ejercicio 4: Manipulación de cadenas
     // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
     // Luego, muestra la longitud del mensaje y el antepenultimo en el mensaje. (.length)
     function manipularCadenas() {
- 
+    
     // Tu código aquí
+    let direccion = parseInt(prompt("Ingrese su dirección:"));
+    let n_casa = parseInt(prompt("Ingrese el número de casa:"));
+    let mensaje = "La dirección es: " + direccion + " " + n_casa;
+    alert("La direccion es: " + mensaje + "/n Cantidad de caracteres:" + mensaje.length() 
+    + "Caracter ante penultimo" + mensaje[mensaje.length -3]);
     }
