1d825666b272c6adab8bd20cea6f3c9639b8a4a5
markVz3C <martinloncon@liceovvh.cl>
2025-03-19 09:27:18 -0300

    ok

--- JavaScript/06_condiciones/script.js
+++ JavaScript/06_condiciones/script.js
@@ -1,7 +1,6 @@
 // Problema 2: Evaluación de Temperatura
 function evaluarTemperatura() {
-    let temperatura = 10; // Cambia este valor para probar diferentes escenarios
-
+    let temperatura = parseInt(prompt("Introduce la temperatura para evaluar:"));
     if (temperatura < 0) {
         alert("Hace frío.");
     } else if (temperatura >= 0 && temperatura < 25) {
@@ -13,22 +12,28 @@ function evaluarTemperatura() {
 
 // Problema 3: Verificación de Usuario y Contraseña
 function verificarUsuario() {
-    let nombreUsuario = "usuario123";
-    let contraseña = "secreto"; 
+    let nombreUsuario =parseInt(prompt("Introduce el nombre de usuario:"));
+    let contraseña = parseInt(prompt("Introduce la contraseña:"));
     if (nombreUsuario === "usuario123" && contraseña === "secreto") {
         alert("Acceso concedido.");
-        } else {
-            alert("Acceso denegado.");
-        }
+    } else {
+        alert("Acceso denegado.");
     }
+}
 
-
-
+// Problema 1: Solicitar un número y verificar si es par o impar
+function verificarParidad() {
+    let numero = parseInt(prompt("Introduce un número para verificar si es par o impar:"));
+    if (numero % 2 === 0) {
+        alert("El número es par.");
+    } else {
+        alert("El número es impar.");
+    }
+}
 
 // Problema 4: Determinar el Signo de un Número
 function determinarSigno() {
-    let numero = 5; // Cambia este valor para probar diferentes escenarios
-
+    let numero = parseInt(prompt("Introduce un número para determinar su signo:"));
     if (numero > 0) {
         alert("El número es positivo.");
     } else if (numero < 0) {
@@ -38,11 +43,9 @@ function determinarSigno() {
     }
 }
 
-
 // Problema 5: Evaluación de Puntuación en un Juego
 function evaluarPuntuacion() {
-    let puntuacion = 85; // Cambia este valor para probar diferentes escenarios
-
+    let puntuacion = parseInt(prompt("Introduce la puntuación del juego:"));
     if (puntuacion >= 90) {
         alert("Excelente.");
     } else if (puntuacion >= 70) {
@@ -50,4 +53,4 @@ function evaluarPuntuacion() {
     } else {
         alert("Necesitas mejorar.");
     }
-} 
+}
