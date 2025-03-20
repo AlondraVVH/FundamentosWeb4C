8cdc7b9173f34e05ba6ea5d9b82fdda7768e4c9c
markVz3C <martinloncon@liceovvh.cl>
2025-03-20 11:17:13 -0300

    ok

---
+++ JavaScript/07_operadores/script.js
@@ -0,0 +1,12 @@
+//problema 6
+function verificacion() {
+    let verificacion = false;
+    let edad = prompt("Ingresa tu edad: ");
+    if (edad >= 18 && verificacion == true) {
+        alert("Acceso permitido");
+    } else {
+        alert("Acceso denegado");
+    }
+
+
+}
